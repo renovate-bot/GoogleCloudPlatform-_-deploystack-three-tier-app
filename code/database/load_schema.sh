a486312f7d4ee57d3c515f94827db13c7ddbc966
bhandarivijay <bhandarivijay@google.com>
2026-01-06 16:08:13 +0000

    chore: Migrate gsutil usage to gcloud storage

--- code/database/load_schema.sh
+++ code/database/load_schema.sh
@@ -2,9 +2,9 @@ PROJECT=$1
 SQLNAME=$2
 
 SQLSERVICEACCOUNT=$(gcloud sql instances describe $SQLNAME --project $PROJECT --format="value(serviceAccountEmailAddress)" | xargs)
-gsutil mb gs://$PROJECT-temp 
-gsutil cp schema.sql gs://$PROJECT-temp/schema.sql
-gsutil iam ch serviceAccount:$SQLSERVICEACCOUNT:objectViewer gs://$PROJECT-temp/
+gcloud storage buckets create gs://$PROJECT-temp 
+gcloud storage cp schema.sql gs://$PROJECT-temp/schema.sql
+gcloud storage buckets add-iam-policy-binding gs://$PROJECT-temp --member=serviceAccount:$SQLSERVICEACCOUNT --role=objectViewer
 gcloud sql import sql $SQLNAME gs://$PROJECT-temp/schema.sql -q
-gsutil rm gs://$PROJECT-temp/schema.sql
-gsutil rb gs://$PROJECT-temp
+gcloud storage rm gs://$PROJECT-temp/schema.sql
+gcloud storage buckets delete gs://$PROJECT-temp
